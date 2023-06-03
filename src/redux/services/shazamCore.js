dcc1acefaa703e3bf0e891d067c237162b0f317c
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-06-03 11:46:00 +0100

    restructure the endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -8,19 +8,6 @@
 // import {createAPI, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '14755ef2e2mshd70daba3a06691ap1c4461jsnf19bfd10f9e2',
-//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-//     }
-//   };
-  
-//   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
